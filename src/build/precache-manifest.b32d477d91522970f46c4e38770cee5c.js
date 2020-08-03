1f883e55162dffb2c1e75ce82d50a828aaa01934
binary00001 <jordansdad253@gmail.com>
2020-08-03 01:14:50 -0700

    first commit

---
+++ src/build/precache-manifest.b32d477d91522970f46c4e38770cee5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff3249f022b5bf405c6b9489be4e65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c3f97d3c591b6a36f4b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3386060592feb4bf997a",
+    "url": "/static/css/main.53b8c378.chunk.css"
+  },
+  {
+    "revision": "7c3f97d3c591b6a36f4b",
+    "url": "/static/js/2.da4ebb7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.da4ebb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3386060592feb4bf997a",
+    "url": "/static/js/main.9242fe9e.chunk.js"
+  },
+  {
+    "revision": "c734195ee93929de00dc",
+    "url": "/static/js/runtime-main.03ec22df.js"
+  }
+]);
